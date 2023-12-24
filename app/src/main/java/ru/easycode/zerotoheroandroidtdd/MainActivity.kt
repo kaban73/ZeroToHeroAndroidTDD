2e99bd1eae13b561bd2ba7ccdd86ca11b900c566
Ivan Borkov <i.borkoff@yandex.ru>
2023-12-24 17:57:57 +0300

    Task007 solved on SaveInstanceState

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,28 +1,31 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import android.content.Context
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var b : ActivityMainBinding
-    private lateinit var sharedPreferences: SharedPreferences
+
+    private lateinit var b:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        b = ActivityMainBinding.inflate(layoutInflater)
+        b= ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
-        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
 
-        val saveText = sharedPreferences.getString("SaveText",b.titleTextView.text.toString())
-        b.titleTextView.text = saveText
         b.changeButton.setOnClickListener{
             b.titleTextView.text = getString(R.string.i_am_an_android_developer)
-
-            val editor = sharedPreferences.edit()
-            editor.putString("SaveText", b.titleTextView.text.toString())
-            editor.apply()
         }
     }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("saveText", b.titleTextView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        b.titleTextView.text = savedInstanceState.getString("saveText")
+    }
+
 }
