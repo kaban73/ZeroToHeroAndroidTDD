7ee6925817f70f62c9015cb2fceb1611cc315b5b
Ivan Borkov <i.borkoff@yandex.ru>
2023-12-27 12:30:26 +0300

    Task009 solved on SaveInstance and OOP

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,15 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.TextView
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
+    private var state : State = State.Initial
     private lateinit var b : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,22 +17,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(b.root)
 
         b.removeButton.setOnClickListener{
-            b.rootLayout.removeView(b.titleTextView)
+            state = State.Removed
+            state.apply(b.rootLayout, b.titleTextView)
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(KEY, b.rootLayout.findViewById<TextView>(R.id.titleTextView)?.toString())
+        outState.putSerializable(KEY, state)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        if (savedInstanceState.getString(KEY) == null)
-            b.rootLayout.removeView(b.titleTextView)
+        state = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            savedInstanceState.getSerializable(KEY, State::class.java) as State
+        } else {
+            savedInstanceState.getSerializable(KEY) as State
+        }
+        state.apply(b.rootLayout, b.titleTextView)
     }
 
     companion object {
         private const val KEY: String = "isExists"
+    }
+}
+
+interface State : Serializable {
+    fun apply(linearLayout: LinearLayout, textView: TextView)
+
+    object Initial : State {
+        override fun apply(linearLayout: LinearLayout, textView: TextView) = Unit
+    }
+
+    object Removed : State {
+        override fun apply(linearLayout: LinearLayout, textView: TextView) {
+            linearLayout.removeView(textView)
+        }
     }
 }
