a23ca3a8aca3db875ff5cd3c7238160ed05d40ae
Ivan Borkov <i.borkoff@yandex.ru>
2023-12-27 12:04:03 +0300

    Task009 solved on SaveInstance and if

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,33 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var b : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+
+        b.removeButton.setOnClickListener{
+            b.rootLayout.removeView(b.titleTextView)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(KEY, b.rootLayout.findViewById<TextView>(R.id.titleTextView)?.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState.getString(KEY) == null)
+            b.rootLayout.removeView(b.titleTextView)
+    }
+
+    companion object {
+        private const val KEY: String = "isExists"
     }
 }
