9ddc8e2d100b7d7ad2ef8001e0862c516c8ac83d
Ivan Borkov <i.borkoff@yandex.ru>
2024-01-01 13:03:57 +0300

    Task014 solved on CountDownTimer(my attempt)

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,38 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var b : ActivityMainBinding
+    private var i : Int = 0
+    private val mCountDownTimer = object : CountDownTimer(3500, 1000) {
+        override fun onTick(millisUntilFinished: Long) {
+            i++
+            b.progressBar.progress = i * 100 / (3500 / 1000)
+        }
+
+        override fun onFinish() {
+            b.progressBar.visibility = View.GONE
+            b.titleTextView.visibility = View.VISIBLE
+            b.actionButton.isEnabled = true
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+
+
+        b.actionButton.setOnClickListener{
+            b.progressBar.visibility = View.VISIBLE
+            b.actionButton.isEnabled = false
+            mCountDownTimer.start()
+        }
     }
 }
