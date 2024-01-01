48c223020877d6fefc86bd94660d22b0c3e25719
Ivan Borkov <i.borkoff@yandex.ru>
2024-01-01 13:23:51 +0300

    Task014 solved on PostDelayed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,7 +2,6 @@ package ru.easycode.zerotoheroandroidtdd
 
 
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
@@ -10,29 +9,20 @@ import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var b : ActivityMainBinding
-    private var i : Int = 0
-    private val mCountDownTimer = object : CountDownTimer(3500, 1000) {
-        override fun onTick(millisUntilFinished: Long) {
-            i++
-            b.progressBar.progress = i * 100 / (3500 / 1000)
-        }
-
-        override fun onFinish() {
-            b.progressBar.visibility = View.GONE
-            b.titleTextView.visibility = View.VISIBLE
-            b.actionButton.isEnabled = true
-        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
 
-
         b.actionButton.setOnClickListener{
             b.progressBar.visibility = View.VISIBLE
             b.actionButton.isEnabled = false
-            mCountDownTimer.start()
+
+            b.actionButton.postDelayed({
+                b.progressBar.visibility = View.GONE
+                b.titleTextView.visibility = View.VISIBLE
+                b.actionButton.isEnabled = true
+            }, 3500)
         }
     }
 }
