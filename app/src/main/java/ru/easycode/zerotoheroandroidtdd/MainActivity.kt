e342635b88f19202ce352e20a69b392555805413
Ivan Borkov <i.borkoff@yandex.ru>
2024-01-03 16:16:26 +0300

    Task016 solved on MVVM and SaveInstanceState

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,45 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var b : ActivityMainBinding
+    private var viewModel : MainViewModel = MainViewModel(
+        LiveDataWrapper.Base(),
+        Repository.Base())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+
+        b.actionButton.setOnClickListener{
+            viewModel.load()
+        }
+
+        viewModel.liveData().observe(this) {uiState ->
+            uiState.apply(b.titleTextView, b.actionButton, b.progressBar)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable(KEY, viewModel)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        viewModel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            savedInstanceState.getSerializable(KEY, MainViewModel::class.java) as MainViewModel
+        } else {
+            savedInstanceState.getSerializable(KEY) as MainViewModel
+        }
+        viewModel.liveData().value?.apply(b.titleTextView, b.actionButton, b.progressBar)
+    }
+
+    companion object {
+        private const val KEY = "viewModelKey"
     }
 }
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/UiState.kt
@@ -0,0 +1,27 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+
+interface UiState {
+    fun apply(textView: TextView, button: Button, progressBar: ProgressBar)
+    object ShowProgress : UiState {
+        override fun apply(textView: TextView, button: Button, progressBar: ProgressBar) {
+            button.isEnabled = false
+            textView.visibility = View.GONE
+            progressBar.visibility = View.VISIBLE
+        }
+
+    }
+
+    object ShowData: UiState {
+        override fun apply(textView: TextView, button: Button, progressBar: ProgressBar) {
+            button.isEnabled = true
+            textView.visibility = View.VISIBLE
+            progressBar.visibility = View.GONE
+        }
+
+    }
+}
