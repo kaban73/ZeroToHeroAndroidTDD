9e5a28dcbac3624d16df0a7f78d21d691f03c78f
Ivan Borkov <i.borkoff@yandex.ru>
2024-01-04 12:20:08 +0300

    Task016 solved on MVVM with App

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,16 +2,16 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var b : ActivityMainBinding
-    private val viewModel = MainViewModel(KeepLiveData.liveData, Repository.Base())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
+
+        val viewModel = (application as App).viewModel
 
         b.actionButton.setOnClickListener{
             viewModel.load()
@@ -21,8 +21,4 @@ class MainActivity : AppCompatActivity() {
             uiState.apply(b.titleTextView, b.actionButton, b.progressBar)
         }
     }
-}
-
-object KeepLiveData {
-    val liveData = LiveDataWrapper.Base()
 }
