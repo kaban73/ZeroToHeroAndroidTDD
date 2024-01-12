c197c5411d37bf888d24cfcec0227e7bb619f48a
Ivan Borkov <i.borkoff@yandex.ru>
2024-01-12 17:17:14 +0300

    Task021 solved on FreezesText

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,25 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var b : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.rootLayout)
+
+        b.inputEditText.setOnClickListener {
+            b.inputEditText.setSelection( b.inputEditText.length())
+        }
+        b.inputEditText.addTextChangedListener {
+            b.actionButton.isEnabled = it.toString() == "min"
+        }
+        b.actionButton.setOnClickListener {
+            b.titleTextView.text = b.inputEditText.text
+            b.inputEditText.setText("")
+        }
     }
 }
