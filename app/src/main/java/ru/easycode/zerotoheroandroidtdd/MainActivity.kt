b764eef62d2c6c53d07044b4af9c45e45443de0a
Ivan Borkov <i.borkoff@yandex.ru>
2024-01-13 12:38:25 +0300

    Task023 solved on CustomAdapter

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,16 +1,43 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-
+    private val views = arrayListOf<String>()
+    private var customAdapter = CustomAdapter(views)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = customAdapter
+        binding.actionButton.setOnClickListener {
+            customAdapter.add(binding.inputEditText.text.toString())
+            binding.inputEditText.setText("")
+        }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable(KEY, customAdapter)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        customAdapter = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            savedInstanceState.getSerializable(KEY, CustomAdapter::class.java) as CustomAdapter
+        } else {
+            savedInstanceState.getSerializable(KEY) as CustomAdapter
+        }
+        binding.recyclerView.adapter = customAdapter
+    }
+
+    companion object {
+        private const val KEY = "key"
     }
 }
