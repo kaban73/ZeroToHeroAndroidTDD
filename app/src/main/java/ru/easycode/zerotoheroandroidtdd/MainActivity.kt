c29c2f1f467b6c8469df3330fc27deb33a161770
Ivan Borkov <i.borkoff@yandex.ru>
2024-01-13 10:54:05 +0300

    Task022 solved on AddView(not my attempt)

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,47 +1,40 @@
 package ru.easycode.zerotoheroandroidtdd
 
 import android.os.Bundle
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var b : ActivityMainBinding
-    private val views : ArrayList<String> = arrayListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.rootLayout)
         b.actionButton.setOnClickListener {
-            val textView = TextView(this)
-            textView.layoutParams =
-                LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT)
-            textView.text = b.inputEditText.text
-            views.add(textView.text.toString())
-            b.contentLayout.addView(textView)
+            addTextView(b.inputEditText.text.toString())
             b.inputEditText.setText("")
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putStringArrayList(KEY, views)
+        val views = b.contentLayout.children.map { (it as TextView).text.toString() }.toList()
+        outState.putStringArrayList(KEY, ArrayList(views))
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         val listViews  = savedInstanceState.getStringArrayList(KEY)
-        listViews?.forEach {
-            val textView = TextView(this@MainActivity)
-            textView.layoutParams =
-                LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT)
-            textView.text = it
-            b.contentLayout.addView(textView)
-        }
+        listViews?.forEach { addTextView(it) }
+    }
+
+    private fun addTextView(text : String) {
+        val textView = TextView(this@MainActivity)
+        textView.text = text
+        b.contentLayout.addView(textView)
     }
 
     companion object {
